34947c4d06ee3498f97be13408b19c6c0f491fad
Халиуллин Марсель <mr.khaliullinmarsel@mail.ru>
2026-09-19 17:27:54 +0300

    чуть изменил третье

--- src/lab01/03_discount_vat.py
+++ src/lab01/03_discount_vat.py
@@ -1,7 +1,7 @@
 """Lab 01, exercise 03."""
-price = float(input('price: '))
-discount = float(input('discount: '))
-vat = float(input('vat: '))
+price = float(input('price: ').replace(',', '.'))
+discount = float(input('discount: ').replace(',', '.'))
+vat = float(input('vat: ').replace(',', '.'))
 base = price * (1 - discount / 100)
 vat_amount = base * (vat/100)
 total = base + vat_amount
